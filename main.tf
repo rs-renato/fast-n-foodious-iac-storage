30c6fb0a5f1700cd7b1f109641aebccd5762a1b5
Renato Rodrigues <spamcares-github@yahoo.com>
2024-01-04 23:56:28 -0300

    - main/remote: configuração de bucket

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   required_version = ">=1.0"
 
   backend "s3" {
-    bucket  = "fast-n-foodious-terraform-storage"
+    bucket  = "fnf-tf-storage"
     key     = "terraform.tfstate"
     region  = "us-east-1"
   }
